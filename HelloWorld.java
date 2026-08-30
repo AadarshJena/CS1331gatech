934a1d154ddfb52afe30ec6e83e6d6668073a895
AadarshJena <aadarsh.jena2008@gmail.com>
2026-08-30 14:10:49 -0400

    Added the first CS 1331 assignment 0.0

---
+++ HelloWorld.java
@@ -0,0 +1,37 @@
+//This is lecture two 
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+        
+        String greeting = "hello World";
+        System.out.println("greeting");
+        /*
+        int year = 2026; //uses 32 bits to store its numbers
+        float temperature = 99.9999f;   //
+        double gpa = 3.9;
+        char noah = 'n';
+        boolean willGTWinTheNatty = false;
+        byte; //8 bits
+        short; // 
+        long debt;      //uses 64 bits to store its values
+        System.out.println("hello world");
+        */
+
+        final boolean willGtWinTheNatty = true;
+
+
+    }
+
+
+
+}
+
+/* 
+
+Hey look I'm a multi-line comment 
+
+n shi
+
+
+*/
